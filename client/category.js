5e744a6ea8d7e79f91f43d09268c1cd1fd09015e
rohit <rohitmahto062@gmail.com>
2025-09-11 22:25:53 +0530

    feat: Add category management; create CategoryCard component and integrate categories into UserDashboard

---
+++ client/category.js
@@ -0,0 +1,24 @@
+import image1 from "./src/assets/andwiches.jpg";
+import image2 from "./src/assets/burgger.jpg";
+import image3 from "./src/assets/chinese.jpg";
+import image4 from "./src/assets/desserts.jpg";
+import image5 from "./src/assets/fast-food.jpg";
+import image6 from "./src/assets/main-course.jpg";
+import image7 from "./src/assets/north-indian-food.jpg";
+import image8 from "./src/assets/pizza.jpg";
+import image9 from "./src/assets/snacks.jpg";
+import image11 from "./src/assets/south-indian.jpg";
+
+export const category = [
+  { category: "Snacks", image: image9 },
+  { category: "Main Course", image: image6 },
+  { category: "Desserts", image: image4 },
+  { category: "Pizza", image: image8 },
+  { category: "Burgers", image: image2 },
+  { category: "Sandwiches", image: image1 },
+  { category: "South Indian", image: image11 },
+  { category: "North Indian", image: image7 },
+  { category: "Chinese", image: image3 },
+  { category: "Fast Food", image: image5 },
+  { category: "All", image: image6 },
+];
--- client/src/components/UserDashboard.jsx
+++ client/src/components/UserDashboard.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
 import Nav from "./Nav";
+import { category } from "../../category";
+import CategoryCard from "./CategoryCard";
+import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 
 const UserDashboard = () => {
+  const categoryScrool = useRef();
+
+  const scroolHandler = (ref, direction) => {
+    if (ref.current) {
+      ref.current.scrollBy({
+        left: direction === "left" ? -200 : 200,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <div>
+    <div className="w-screen min-h-screen flex flex-col gap-5 items-center bg-[#fff9f6] overflow-y-auto">
       <Nav />
+      <div className="w-full max-w-6xl flex flex-col gap-5 items-start p-[10px]">
+        <h1 className="text-gray-800 text-2xl sm:text-3xl">
+          Inspiration for your first order
+        </h1>
+
+        <div className="w-full relative">
+          <button
+            className=" absolute left-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 "
+            onClick={() => scroolHandler(categoryScrool, "left")}
+          >
+            <FaChevronLeft />
+          </button>
+          <div
+            className="w-full flex overflow-x-auto gap-4 pb-2 "
+            ref={categoryScrool}
+          >
+            {category.map((cat, index) => (
+              <CategoryCard data={cat} key={index} />
+            ))}
+          </div>
+          <button
+            className=" absolute right-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 "
+            onClick={() => scroolHandler(categoryScrool, "right")}
+          >
+            <FaChevronRight />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
