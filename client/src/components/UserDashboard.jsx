77f5abdfdce9b97a999b1fbf353658764434b0a5
rohit <rohitmahto062@gmail.com>
2025-09-12 12:12:03 +0530

    feat: Enhance UserDashboard with scroll buttons visibility; implement dynamic button display based on scroll position

--- client/src/components/UserDashboard.jsx
+++ client/src/components/UserDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Nav from "./Nav";
 import { category } from "../../category";
 import CategoryCard from "./CategoryCard";
@@ -7,15 +7,52 @@ import { FaChevronRight } from "react-icons/fa";
 
 const UserDashboard = () => {
   const categoryScrool = useRef();
+  const [showLeftcatButton, setShowLeftCatButton] = useState(false);
+  const [showRightcatButton, setShowRightCatButton] = useState(false);
+
+  const handleShowButton = (ref, showLeftButton, showRightButton) => {
+    const element = ref.current;
+    // console.log(element.scrollLeft);
+    if (element) {
+      showLeftButton(element.scrollLeft > 0);
+      // console.log(element.clientWidth);
+      showRightButton(
+        element.clientWidth + element.scrollLeft < element.scrollWidth
+      );
+    }
+  };
 
   const scroolHandler = (ref, direction) => {
     if (ref.current) {
       ref.current.scrollBy({
-        left: direction === "left" ? -200 : 200,
+        left:
+          direction === "left"
+            ? -ref.current.clientWidth
+            : ref.current.clientWidth,
         behavior: "smooth",
       });
     }
   };
+
+  useEffect(() => {
+    if (categoryScrool.current) {
+      const checkButtons = () =>
+        handleShowButton(
+          categoryScrool,
+          setShowLeftCatButton,
+          setShowRightCatButton
+        );
+
+      // Run initially
+      checkButtons();
+
+      categoryScrool.current.addEventListener("scroll", checkButtons);
+
+      return () => {
+        categoryScrool.current.removeEventListener("scroll", checkButtons);
+      };
+    }
+  }, []);
 
   return (
     <div className="w-screen min-h-screen flex flex-col gap-5 items-center bg-[#fff9f6] overflow-y-auto">
@@ -26,12 +63,15 @@ const UserDashboard = () => {
         </h1>
 
         <div className="w-full relative">
-          <button
-            className=" absolute left-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 "
-            onClick={() => scroolHandler(categoryScrool, "left")}
-          >
-            <FaChevronLeft />
-          </button>
+          {showLeftcatButton && (
+            <button
+              className=" absolute left-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 "
+              onClick={() => scroolHandler(categoryScrool, "left")}
+            >
+              <FaChevronLeft />
+            </button>
+          )}
+
           <div
             className="w-full flex overflow-x-auto gap-4 pb-2 "
             ref={categoryScrool}
@@ -40,12 +80,14 @@ const UserDashboard = () => {
               <CategoryCard data={cat} key={index} />
             ))}
           </div>
-          <button
-            className=" absolute right-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 "
-            onClick={() => scroolHandler(categoryScrool, "right")}
-          >
-            <FaChevronRight />
-          </button>
+          {showRightcatButton && (
+            <button
+              className=" absolute right-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 "
+              onClick={() => scroolHandler(categoryScrool, "right")}
+            >
+              <FaChevronRight />
+            </button>
+          )}
         </div>
       </div>
     </div>
