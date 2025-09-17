2502937f5627da41425f737e9e882c55ed231be7
rohit <rohitmahto062@gmail.com>
2025-09-18 01:04:17 +0530

    feat: Enhance CheckOut component with address input binding, location fetching, and draggable marker functionality

--- client/src/pages/CheckOut.jsx
+++ client/src/pages/CheckOut.jsx
@@ -3,13 +3,73 @@ import { useNavigate } from "react-router-dom";
 import { IoLocationSharp } from "react-icons/io5";
 import { IoSearchSharp } from "react-icons/io5";
 import { TbCurrentLocation } from "react-icons/tb";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
-import { useSelector } from "react-redux";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { useDispatch, useSelector } from "react-redux";
 import "leaflet/dist/leaflet.css";
+import { setAddress, setLocation } from "../redux/mapSlice";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function RecenterMap({ location }) {
+  const map = useMap();
+  map.setView([location.lat, location.lon], 16, { animate: true });
+}
 
 function CheckOut() {
-  const navigate = useNavigate();
+  const [addressInput, setAddressInput] = useState("");
   const { location, address } = useSelector((state) => state.map);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const apiKey = import.meta.env.VITE_GEOAPIKEY;
+
+  const onDragEnd = (e) => {
+    console.log(e.target._latlng);
+    const { lat, lng } = e.target._latlng;
+    dispatch(setLocation({ lat, lon: lng }));
+    getLocationByLatLon(lat, lng);
+  };
+  const getLocationByLatLon = async (latitude, longitude) => {
+    try {
+      const result = await axios.get(
+        `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${apiKey}`
+      );
+      dispatch(setAddress(result.data?.results[0]?.address_line2));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      //   console.log(position);
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      dispatch(setLocation({ lat: latitude, lon: longitude }));
+      getLocationByLatLon(latitude, longitude);
+    });
+  };
+
+  const getLatLonByAddress = async () => {
+    try {
+      const result = await axios.get(
+        `https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(
+          addressInput
+        )}&apiKey=${apiKey}`
+      );
+      console.log(result.data.features[0].properties);
+      const { lat, lon } = result.data.features[0].properties;
+      dispatch(setLocation({ lat, lon }));
+      dispatch(setAddress(addressInput));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    setAddressInput(address);
+  }, [address]);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#fff9f6] p-6 ">
       <div
@@ -31,12 +91,19 @@ function CheckOut() {
               type="text"
               className="flex-1 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-green-600 p-2 "
               placeholder="Enter your delivery address"
-              value={address}
+              value={addressInput}
+              onChange={(e) => setAddressInput(e.target.value)}
             />
-            <button className="bg-green-500 hover:bg-green-600 text-white px-3 py-2 rounded-lg flex items-center justify-center cursor-pointer">
+            <button
+              className="bg-green-500 hover:bg-green-600 text-white px-3 py-2 rounded-lg flex items-center justify-center cursor-pointer"
+              onClick={getLatLonByAddress}
+            >
               <IoSearchSharp size={20} />
             </button>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded-lg flex items-center justify-center cursor-pointer">
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded-lg flex items-center justify-center cursor-pointer"
+              onClick={getCurrentLocation}
+            >
               <TbCurrentLocation size={20} />
             </button>
           </div>
@@ -52,7 +119,12 @@ function CheckOut() {
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[location.lat, location.lon]} />
+                <RecenterMap location={location} />
+                <Marker
+                  position={[location.lat, location.lon]}
+                  draggable
+                  eventHandlers={{ dragend: onDragEnd }}
+                />
               </MapContainer>
             </div>
           </div>
