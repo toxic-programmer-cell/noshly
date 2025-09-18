fceb37e5e2562d90c01d36836851570fd1f0fb6f
rohit <rohitmahto062@gmail.com>
2025-09-19 00:23:48 +0530

    feat: add gh-pages deployment script and update dependencies
    - Added "predeploy" script to build the project before deployment.
    - Updated "deploy" script to use gh-pages for deployment.
    - Added "gh-pages" as a dependency in package.json.
    - Updated server URL in App.jsx to point to the production backend.
    - Set the basename for BrowserRouter in main.jsx to match the deployment path.
    - Updated allowed origins in server/index.js to include the new deployment URL.
    - Updated package-lock.json with new dependencies and versions.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/noshly">
     <Provider store={store}>
       <App />
     </Provider>
--- server/index.js
+++ server/index.js
@@ -11,9 +11,15 @@ import itemRouter from "./routes/items.routes.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://toxic-programmer-cell.github.io",
+];
 app.use(
+  "*",
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
