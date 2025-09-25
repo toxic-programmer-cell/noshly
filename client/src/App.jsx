804636b5860925b928d3c07a3533ac0ba7c3a2b4
Rohit Kumar Mahto <rohitmahto062@gmail.com>
2025-09-25 22:53:31 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import useGetShopByCity from "./hooks/useGetShopByCity";
 import useGetItemByCity from "./hooks/useGetItemByCity";
 import CartPage from "./pages/CartPage";
 import CheckOut from "./pages/CheckOut";
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://noshly-backend.onrender.com";
 
 function App() {
   useGetCurrentUser();
